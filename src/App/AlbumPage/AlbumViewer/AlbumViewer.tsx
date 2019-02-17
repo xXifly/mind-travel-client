cf89ec338489ac616c3139d1e76d863b5160e759
MaximeMD <maxime.martydessus@gmail.com>
2019-02-17 22:41:51 +0100

    improve gallery

--- src/App/AlbumPage/AlbumViewer/AlbumViewer.tsx
+++ src/App/AlbumPage/AlbumViewer/AlbumViewer.tsx
@@ -17,8 +17,16 @@ const AlbumViewer = (props: IAlbumViewerProps) => {
   //   const picturesGallery: Picture[] = [];
   const picturesGallery: any = [];
 
+  let widthIterator = 0;
+
   props.album.pictures.forEach((pictureKey: string) => {
     let randomRatio = Math.floor(Math.random() * 0.5) + 0.5;
+
+    //generate a 'random' width to get a masonry effect
+    widthIterator = (widthIterator + 1) % 10;
+    const picWidth = [200, 250, 300, 215, 255, 180, 225, 280, 240, 275][
+      widthIterator
+    ];
 
     picturesGallery.push({
       src:
@@ -29,10 +37,7 @@ const AlbumViewer = (props: IAlbumViewerProps) => {
       thumbnail:
         'http://192.168.1.42:8080/api/pictures/thumb/' +
         encodeURIComponent(pictureKey),
-      //generate a random width to get a masonry effect
-      thumbnailWidth: [200, 225, 250, 275, 300][
-        Math.floor(Math.random() * 4) + 1
-      ],
+      thumbnailWidth: picWidth,
       thumbnailHeight: 200
     });
   });
